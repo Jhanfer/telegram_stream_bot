3bea068eb99f2222ac5f421f0610bd44ade8a8fd
Jhanfer <jhafer73@gmail.com>
2024-11-20 15:31:10 +0100

    Test sin flask y usando http server, socketserver

--- code/telegram_bot.py
+++ code/telegram_bot.py
@@ -4,19 +4,42 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Updater, Application
 from twitch_api import TwitchAPI
 import asyncio
-from flask import Flask, request
+import http.server
+import socketserver
 import telegram
+
+
+api = TwitchAPI("unweonmais")
+dotenv.load_dotenv()
+API_KEY = os.environ.get("API_KEY")
+bot = telegram.Bot(token=API_KEY)
+
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        length = int(self.headers['Content-Length'])
+        body = self.rfile.read(length)
+        update = telegram.Update.de_json(body.decode('utf-8'), bot)
+
+        # Procesar la actualización (ej: responder a un mensaje)
+        if update.message:
+            chat_id = update.message.chat.id
+            message_text = update.message.text
+            bot.send_message(chat_id=chat_id, text=f"Has dicho: {message_text}")
+
+        self.send_response(200)
+        self.end_headers()
+
+
+
+
+
 
 contadores = {
     "contador1":0,
     "contador2":0
 }
-api = TwitchAPI("Rubius")
-dotenv.load_dotenv()
-API_KEY = os.environ.get("API_KEY")
-flask = Flask(__name__)
-app = ApplicationBuilder().token(API_KEY).build()
-bot = telegram.Bot(API_KEY)
+
 
 async def hola(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Todo bien, {update.effective_user.first_name} pa? ")
@@ -45,43 +68,30 @@ async def stream_alert(context: ContextTypes.DEFAULT_TYPE) -> None:
     if chat_id:
         data = api.get_data()
         if api.is_live() == True and contador1["contador1"] < 1:
-            await context.bot.send_message(chat_id, f'{api.user} está en vivo! \n{data["title"]} \n{data["game_name"]} \n🔴{int(data["viewer_count"]):,.2f}🔴 \nhttps://twitch.tv/{api.user}')
+            await context.bot.send_message(chat_id, f'{api.user} está en vivo! \n{data["title"]} \n{data["game_name"]} \n🔴{int(data["viewer_count"]):,}🔴 \nhttps://twitch.tv/{api.user} \n@baltaok @Legna')
             contador1["contador1"] += 1
 
         elif api.is_live() == False:
             if contador1["contador1"] > 0 or estado_anterior["is_live"] == True:
                 contador1["contador1"] = 0
                 estado_anterior["is_live"] = False
-                await context.bot.send_message(chat_id, f'Ahora mismo {api.user} no está en vivo!')
-
-
-@flask.route('/' + bot.token, methods=['POST'])
-def receive_update():
-    # Obtener la actualización de Telegram
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-    # Procesar la actualización (ej: responder a un mensaje)
-    if update.message:
-        chat_id = update.message.chat.id
-        message_text = update.message.text
-        bot.send_message(chat_id=chat_id, text=f"Has dicho: {message_text}")
-
-    return 'ok'
-
-
+                await context.bot.send_message(chat_id, f'Ahora mismo {api.user} no está en vivo! \n@baltaok @Legna')
 
 
 
 if __name__ == "__main__":
     
-
+    with socketserver.TCPServer(("", 8080), MyHandler) as httpd:
+        print("serving at port", 8080)
     
-    
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("hola", hola))
-
-    flask.run(host='0.0.0.0', port=8080)
-    app.run_polling(allowed_updates=Update.ALL_TYPES, close_loop=True,drop_pending_updates=True)
+        app = ApplicationBuilder().token(API_KEY).build()
+        
+        app.add_handler(CommandHandler("start", start))
+        app.add_handler(CommandHandler("hola", hola))
 
 
+        app.run_polling(allowed_updates=Update.ALL_TYPES, close_loop=True,drop_pending_updates=True)
+
+
+        httpd.serve_forever()
 
